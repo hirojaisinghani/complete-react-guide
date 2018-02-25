370c2215d6c3f8ccc47a3c861c93ce329d83ae89
Hiro J <hiro.jaisinghani@solutionanalysts.com>
2018-02-25 18:22:55 +0530

    [complete]: Dynamic looping display

--- src/App.js
+++ src/App.js
@@ -8,75 +8,89 @@ class App extends Component {
     Persons: [
       {
         name: "Hiro Jaisinghani",
-        age:30
+        age:30,
+        id:'sdasdasd',
       },
       {
         name: "Sahil Jaisinghani",
-        age:12
+        age:12,
+        id:'sdasdsd',
       },
       {
         name: "Dheeraj Jaisinghani",
-        age:2
+        age:2,
+        id:'sdasdaghsd',
       }
     ],
     otherState: 'some other value',
+    showPersons: false,
   };
-  switchNameHandler = (newName) => {
-    this.setState(
-      {
-        Persons: [
-          {
-            name: "Hiro Jaisinghani",
-            age:28
-          },
-          {
-            name: newName,
-            age:23
-          },
-          {
-            name: "Dheeraj Jaisinghani",
-            age:10
-          }
-        ]
-      }
-    );
+  
+  deletePersonHandler = (personIndex) => {
+
+    const persons = this.state.Persons;
+    persons.splice(personIndex, 1);
+    this.setState({Persons:persons});
+
+  }
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.Persons.findIndex(p=>{
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.Persons[personIndex]
+    };
+    const persons = [...this.state.Persons];
+    person.name = event.target.value;
+    persons[personIndex] = person;
+    this.setState({
+      Persons:persons
+    })
+
   };
-  nameChangedHandler = (event) => {
-    this.setState( {
-      Persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
-  };
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
+  
   render()  {
     const style = {
       backgroundColor: 'blue',
       border: '1px solid blue',
       cursor: 'pointer',
     }
+    let  Persons = null;
+
+    if(this.state.showPersons) {
+
+      Persons = ( 
+        <div>
+          {this.state.Persons.map((person, index) => {
+            return <Person 
+             name={person.name} 
+            age={person.age}
+            click={() => this.deletePersonHandler(index)}
+            key={person.id}
+            changed={(event)=>this.nameChangedHandler(event, person.id)}> 
+            </Person>
+          }
+          )}
+       </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button style={style} onClick={()=>this.switchNameHandler('Maximilian!!')}>Swich Name</button>
-        <Person 
-          name={this.state.Persons[0].name} 
-          age={this.state.Persons[0].age}
-          click={this.switchNameHandler.bind(this, 'Mahesh')}> 
-        </Person>
-        <Person
-          name={this.state.Persons[1].name} 
-          age={this.state.Persons[1].age}
-          changed={this.nameChangedHandler}>
-          Hobiess are : Football, Surfing, BasketBall
-      </Person>
-        <Person 
-          name={this.state.Persons[2].name} 
-          age={this.state.Persons[2].age}> 
-       </Person>
-        
+        <button style={style} onClick={()=>this.switchNameHandler('Maximilian!!')}>Swich Name</button> <br/>
+        <button onClick={this.togglePersonHandler}>Display Person Toogle</button>
+        <div>
+       
+       {Persons}
+       </div>
+       
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,7 +7,7 @@ const person = (props) => {
         <div className="person">
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
-        <input type="text" onChange={props.changed} />
+        <input type="text" onChange={props.changed}  />
       </div>
     );
 };
